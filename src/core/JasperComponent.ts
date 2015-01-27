c513cbb0a6f0df5ccac5f67e8c6f80adbec230b7
Bukharin V <buhv@skbkontur.ru>
2015-01-27 14:36:36 +0500

    Create wrapper of angular's scope $watch, $watchGroup, $watchCollection

--- src/core/JasperComponent.ts
+++ src/core/JasperComponent.ts
@@ -1,7 +1,7 @@
 module jasper.core {
     export class JasperComponent {
 
-        private $$scope: ng.IScope;
+        private $$scope:ng.IScope;
 
         protected $digest() {
             this.ensureScope();
@@ -15,13 +15,32 @@ module jasper.core {
             this.$$scope.$apply();
         }
 
-        protected $on(eventName: string, listener: (event: ng.IAngularEvent, ...args: any[]) => any){
+        protected $on(eventName:string, listener:(event:ng.IAngularEvent, ...args:any[]) => any) {
             this.ensureScope();
             this.$$scope.$on(eventName, listener);
         }
 
-        private ensureScope(){
-            if(!this.$$scope)
+        protected $watch(watchExpression:(scope:ng.IScope) => any,
+                         listener?:(newValue:any, oldValue:any, scope:ng.IScope)=>any,
+                         objectEquality?:boolean) {
+            this.ensureScope();
+            this.$$scope.$watch(watchExpression, listener, objectEquality);
+        }
+
+        protected $watchCollection(watchExpression: (scope: ng.IScope) => any,
+                                   listener: (newValue: any, oldValue: any, scope: ng.IScope) => any) {
+            this.ensureScope();
+            this.$$scope.$watchCollection(watchExpression, listener);
+        }
+
+        protected $watchGroup(watchExpressions: { (scope: ng.IScope): any }[],
+                              listener: (newValue: any, oldValue: any, scope: ng.IScope) => any) {
+            this.ensureScope();
+            this.$$scope.$watchGroup(watchExpressions, listener);
+        }
+
+        private ensureScope() {
+            if (!this.$$scope)
                 throw '$$scope not initialized';
         }
 
